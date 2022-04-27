185424fa90934a5e22713a1d1a252687959284f8
AdiCahyaSaputra <adi.JSONparse@gmail.com>
2022-04-27 21:15:13 +0700

    auth menggunakan jwt

---
+++ pages/api/auth/login.js
@@ -0,0 +1,30 @@
+import db from "../../../libs/db"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+export default async function handler(req, res) {
+  if(req.method !== "POST") return res.status(405).end()
+  
+  const { email, password } = req.body
+  const user = await db('users')
+    .where({ email })
+    .first()
+    
+  if(!user) return res.status(401).end() // 401 = unauthorized
+  
+  const isAuthUser = await bcrypt.compare(password, user.password) // (plainText, passwordHashed) return boolean
+  if(!isAuthUser) return res.status(401).end()
+  
+  const token = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, "iniTokenRahasiaKu", { expiresIn: '7d' })
+  
+  // (user, tokenRahasia, option atau callback) kalo pake callback nanti jadi nya async btw
+  // return json web token
+  
+  res.status(200).json({
+    msg: "Login",
+    token
+  })
+}
---
+++ pages/api/auth/register.js
@@ -0,0 +1,24 @@
+import db from "../../../libs/db"
+import bcrypt from "bcryptjs"
+
+export default async function handler(req, res) {
+  if(req.method !== "POST") return res.status(405).end()
+  
+  const { email, password } = req.body
+  
+  const salt = bcrypt.genSaltSync(10) // generate salt / str random dengan level kesulitan 10
+  const passwordHash = bcrypt.hashSync(password, salt) // ('strPwNya', salt) return nya pw dengan strRandom + salt nya
+  
+  const registerId = await db('users').insert({
+    email, password: passwordHash
+  })
+  
+  const user = await db('users')
+    .where({ id: registerId })
+    .first()
+  
+  res.status(200).json({
+    msg: "Register",
+    user
+  })
+}
